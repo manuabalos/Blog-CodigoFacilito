1e18c7260f09e8437f9a156a01cc161288d6d63f
Manuel Ábalos Serrano <manu.abalos@gmail.com>
2015-08-03 12:51:10 +0200

    Añadida gema Devise y configurandola Parte 1 de 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "welcome#index"
   resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
