f71fe0a1a1804ddfcab83322d22ad9a32ec5fa3b
Manuel Ábalos Serrano <manu.abalos@gmail.com>
2015-08-03 12:24:18 +0200

    Añadido vistas parciales y el metodo edit y update de Articulos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,10 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
 	#POST /articles
 	def create
 		@article = Article.new article_params
@@ -30,6 +34,16 @@ class ArticlesController < ApplicationController
 		@article.destroy
 
 		redirect_to articles_path
+	end
+
+	#PUT /articles/:id
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render :edit
+		end
 	end
 
 	private
