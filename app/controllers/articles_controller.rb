50d367191cfde35d06f4ee4de62de7383c1e0740
Manuel Ábalos Serrano <manu.abalos@gmail.com>
2015-08-03 11:13:54 +0200

    Creado vista y metodos para crear Articulos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,28 @@
 class ArticlesController < ApplicationController
+	#GET /articles
 	def index
 		@articles = Article.all
 	end
 
+	#GET /articles/:id
 	def show
 		@article = Article.find(params[:id])
 	end
+
+	#GET /articles/new
+	def new
+		@article = Article.new
+	end
+
+	#POST /articles
+	def create
+		@article = Article.new article_params
+		@article.save
+		redirect_to @article
+	end
+
+	private
+    def article_params
+        params.require(:article).permit(:title, :body)
+    end
 end
