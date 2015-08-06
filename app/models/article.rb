af56939e71086289022812906216ef5aa75cf513
Manuel Ábalos Serrano <manu.abalos@gmail.com>
2015-08-06 20:14:54 +0200

    Añadido metodos para articulos y contador de visitas

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,17 @@ class Article < ActiveRecord::Base
 	belongs_to :user
 	validates :title, presence: true, uniqueness: true
 	validates :body, presence: true, length: { minimum: 20 }
+
+	before_save :set_visits_count
+
+	def update_visits_count
+		self.save if self.visits_count.nil?
+		self.update(visits_count: self.visits_count + 1)
+
+	end
+	private
+	def set_visits_count
+		self.visits_count ||= 0
+	end
+
 end
