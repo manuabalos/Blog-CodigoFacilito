eb5b6ce833fecacc6ed3f7e9ec2f81b3e31a88fe
Manuel Ábalos Serrano <manu.abalos@gmail.com>
2015-08-08 21:41:27 +0200

    Opcion de subir imagenes usando Paperclip gem

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -53,6 +53,6 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
     def article_params
-        params.require(:article).permit(:title, :body)
+        params.require(:article).permit(:title, :body, :cover)
     end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,14 @@
 class Article < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
+
 	validates :title, presence: true, uniqueness: true
 	validates :body, presence: true, length: { minimum: 20 }
 
 	before_save :set_visits_count
+
+	has_attached_file :cover, styles: { medium: "1280x720", thumb: "800x600"}
+	validates_attachment_content_type :cover, content_type: /\Aimage\/.*\Z/
 
 	def update_visits_count
 		self.save if self.visits_count.nil?
