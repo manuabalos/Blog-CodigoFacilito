7d7dc2900466f7a30124a2a5e1bfb5bb382746c9
Manuel Ábalos Serrano <manu.abalos@gmail.com>
2015-08-09 15:16:29 +0200

    Creado el modelo Category usando scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150808180433) do
+ActiveRecord::Schema.define(version: 20150809124900) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20150808180433) do
   end
 
   add_index "articles", ["user_id"], name: "index_articles_on_user_id"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "color"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
