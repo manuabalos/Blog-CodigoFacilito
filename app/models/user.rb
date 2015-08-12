9de1e7cc46cbe0f2f37346c4d4e943f03e9c5ce3
Manuel Ábalos Serrano <manu.abalos@gmail.com>
2015-08-12 13:59:10 +0200

    Añadidos modulos concerns para establecer permisos a los usuarios

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :articles
-  has_many :comments
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :articles
+  has_many :comments
+  include PermissionsConcern
 end
